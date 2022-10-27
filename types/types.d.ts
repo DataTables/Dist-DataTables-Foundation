24569b8b820384fe5d0785c41792026887ca7deb
Allan Jardine <allan@datatables.net>
2022-10-27 15:13:15 +0000

    Update typing information to latest typing integration for DataTables

---
+++ types/types.d.ts
@@ -0,0 +1,21 @@
+
+// Dist-DataTables-Foundation integration with Foundation exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
+
+// Dist-DataTables-Foundation integration with Foundation exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
+
+// Dist-DataTables-Foundation integration with Foundation exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
