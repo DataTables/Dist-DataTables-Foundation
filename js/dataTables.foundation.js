4db62e9d1993b0c93ee4cd114a014b4359b50587
Allan Jardine <allan@datatables.net>
2026-05-07 15:33:58 +0000

    Updating package for DataTables 3

--- js/dataTables.foundation.js
+++ js/dataTables.foundation.js
@@ -1,172 +1,138 @@
-/*! DataTables Foundation integration
+/*! DataTables Foundation integration 3.0.0-beta.2
  * © SpryMedia Ltd - datatables.net/license
  */
 
-(function( factory ){
-	if ( typeof define === 'function' && define.amd ) {
+(function(factory){
+	if (typeof define === 'function' && define.amd) {
 		// AMD
-		define( ['jquery', 'datatables.net'], function ( $ ) {
-			return factory( $, window, document );
-		} );
+		define(['datatables.net'], function (dt) {
+			return factory(window, document, dt);
+		});
 	}
-	else if ( typeof exports === 'object' ) {
+	else if (typeof exports === 'object') {
 		// CommonJS
-		var jq = require('jquery');
-		var cjsRequires = function (root, $) {
-			if ( ! $.fn.dataTable ) {
-				require('datatables.net')(root, $);
+		var cjsRequires = function (root) {
+			if (! root.DataTable) {
+				require('datatables.net')(root);
 			}
 		};
 
 		if (typeof window === 'undefined') {
-			module.exports = function (root, $) {
-				if ( ! root ) {
+			module.exports = function (root) {
+				if (! root) {
 					// CommonJS environments without a window global must pass a
 					// root. This will give an error otherwise
 					root = window;
 				}
 
-				if ( ! $ ) {
-					$ = jq( root );
-				}
-
-				cjsRequires( root, $ );
-				return factory( $, root, root.document );
+				cjsRequires(root);
+				return factory(root, root.document, root.DataTable);
 			};
 		}
 		else {
-			cjsRequires( window, jq );
-			module.exports = factory( jq, window, window.document );
+			cjsRequires(window);
+			module.exports = factory(window, window.document, window.DataTable);
 		}
 	}
 	else {
 		// Browser
-		factory( jQuery, window, document );
+		factory(window, document, window.DataTable);
 	}
-}(function( $, window, document ) {
+}(function(window, document, DataTable) {
 'use strict';
-var DataTable = $.fn.dataTable;
 
 
-
-/**
- * DataTables integration for Foundation. This requires Foundation 5.
- *
- * This file sets the defaults and adds options to DataTables to style its
- * controls using Foundation. See https://datatables.net/manual/styling/foundation
- * for further information.
- */
-
-$.extend( true, DataTable.ext.classes, {
-	container: "dt-container dt-foundation",
-	layout: {
-		row: 'grid-x',
-		cell: 'flex-container align-justify align-middle',
-		tableRow: 'dt-layout-table',
-		tableCell: 'cell small-12',
-		start: 'dt-layout-start cell shrink',
-		end: 'dt-layout-end cell shrink',
-		full: 'dt-layout-full cell'
-	},
-	processing: {
-		container: "dt-processing panel callout"
-	}
-} );
-
-
+DataTable.util.object.assignDeep(DataTable.ext.classes, {
+    container: 'dt-container dt-foundation',
+    layout: {
+        row: 'grid-x',
+        cell: 'flex-container align-justify align-middle',
+        tableRow: 'dt-layout-table',
+        tableCell: 'cell small-12',
+        start: 'dt-layout-start cell shrink',
+        end: 'dt-layout-end cell shrink',
+        full: 'dt-layout-full cell'
+    },
+    processing: {
+        container: 'dt-processing panel callout'
+    }
+});
 /* Set the defaults for DataTables initialisation */
-$.extend( true, DataTable.defaults, {
-	renderer: 'foundation'
-} );
-
+DataTable.util.object.assignDeep(DataTable.defaults, {
+    renderer: 'foundation'
+});
 DataTable.ext.renderer.pagingButton.foundation = function (settings, buttonType, content, active, disabled) {
-	var btnClasses = [];
-	var li;
-
-	if (buttonType === 'ellipsis') {
-		// No `a` tag for ellipsis
-		li = $('<li>', {
-			class: 'ellipsis'
-		});
-
-		return {
-			display: li,
-			clicker: li
-		};
-	}
-	else if (active || disabled) {
-		// No `a` tag for current or disabled
-		li = $('<li>', {
-			class: active
-				? 'current'
-				: 'disabled ' + btnClasses.join(' ')
-		}).html(content);
-
-		return {
-			display: li,
-			clicker: li
-		};
-	}
-
-	li = $('<li>').addClass(btnClasses.join(' '));
-	var a = $('<a>', {
-		'href': '#'
-	})
-		.html(content)
-		.appendTo(li);
-
-	return {
-		display: li,
-		clicker: a
-	};
+    var btnClasses = [];
+    var li;
+    if (buttonType === 'ellipsis') {
+        // No `a` tag for ellipsis
+        li = DataTable.Dom.c('li').classAdd('ellipsis');
+        return {
+            display: li.get(0),
+            clicker: li.get(0)
+        };
+    }
+    else if (active || disabled) {
+        // No `a` tag for current or disabled
+        li = DataTable.Dom
+            .c('li')
+            .classAdd(active ? 'current' : 'disabled ' + btnClasses.join(' '))
+            .html(content);
+        return {
+            display: li.get(0),
+            clicker: li.get(0)
+        };
+    }
+    li = DataTable.Dom.c('li').classAdd(btnClasses.join(' '));
+    var a = DataTable.Dom.c('a').attr('href', '#').html(content).appendTo(li);
+    return {
+        display: li.get(0),
+        clicker: a.get(0)
+    };
 };
-
 DataTable.ext.renderer.pagingContainer.foundation = function (settings, buttonEls) {
-	return $('<ul/>').addClass('pagination').append(buttonEls);
+    return DataTable.Dom
+        .c('ul')
+        .classAdd('pagination')
+        .append(buttonEls)
+        .get(0);
 };
-
-DataTable.ext.renderer.layout.foundation = function ( settings, container, items ) {
-	var classes = settings.oClasses.layout;
-	var row = $('<div/>')
-		.attr('id', items.id || null)
-		.addClass(items.className || classes.row)
-		.appendTo( container );
-
-	DataTable.ext.renderer.layout._forLayoutRow(items, function (key, val) {
-		if (key === 'id' || key === 'className') {
-			return;
-		}
-
-		var klass = '';
-		var style = {};
-
-		if (val.table) {
-			row.addClass(classes.tableRow);
-			klass += classes.tableCell + ' ';
-		}
-
-		if (key === 'start') {
-			klass += classes.start;
-		}
-		else if (key === 'end') {
-			klass += classes.end;
-			style.marginLeft = 'auto';
-		}
-		else {
-			klass += classes.full;
-		}
-
-		$('<div/>')
-			.attr({
-				id: val.id || null,
-				"class": val.className
-					? val.className
-					: classes.cell + ' ' + klass
-			})
-			.css(style)
-			.append( val.contents )
-			.appendTo( row );
-	} );
+DataTable.ext.renderer.layout.foundation = function (settings, container, items) {
+    var classes = settings.classes.layout;
+    var row = DataTable.Dom
+        .c('div')
+        .attr('id', items.id || null)
+        .classAdd(items.className || classes.row)
+        .appendTo(container);
+    DataTable.ext.rendererDisplayRowCells(items, function (key, val) {
+        var klass = '';
+        var style = {};
+        if (val.table) {
+            row.classAdd(classes.tableRow);
+            klass += classes.tableCell + ' ';
+        }
+        if (key === 'start') {
+            klass += classes.start;
+        }
+        else if (key === 'end') {
+            klass += classes.end;
+            style.marginLeft = 'auto';
+        }
+        else {
+            klass += classes.full;
+        }
+        DataTable.Dom
+            .c('div')
+            .attr({
+            id: val.id || null,
+            class: val.className
+                ? val.className
+                : classes.cell + ' ' + klass
+        })
+            .css(style)
+            .append(val.contents)
+            .appendTo(row);
+    });
 };
 
 
